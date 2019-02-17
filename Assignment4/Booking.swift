f81695f3ef679584623aa2719f1aed140e2ccf5c
Sissi Chen <chenxysissi@gmail.com>
2019-02-17 17:34:48 -0500

    Update Booking.swift

--- Assignment4/Booking.swift
+++ Assignment4/Booking.swift
@@ -7,17 +7,19 @@
 //
 
 import Foundation
+var calendar = Calendar(identifier:Calendar.Identifier.iso8601)
+
 class Booking{
     var id: Int = bookingId
     var customer: Customer?
     var movie: Movie?
     var quantity: Int?
-    var bookingDate: String?
-    var returnDate: String?
+    var bookingDate: DateComponents?
+    var returnDate: DateComponents?
     static var bookingId:Int = 1
     
     init(){}
-    init(BookingDate:String,ReturnDate:String, Customer:Customer, Movie:Movie, Quantity:Int){
+    init(BookingDate:DateComponents,ReturnDate:DateComponents, Customer:Customer, Movie:Movie, Quantity:Int){
         Booking.bookingId += 1
         self.customer = Customer
         self.movie = Movie
@@ -36,8 +38,8 @@ func CreateBooking(){
         var movieName:String = ""
         var customerName:String = ""
         var quantity:Int = 0
-        var bookingDate:String = ""
-        var returnDate:String = ""
+        var bookingDate:DateComponents = DateComponents(calendar:calendar,year:2019,month:1,day:1)
+        var returnDate:DateComponents = DateComponents(calendar:calendar,year:2019,month:1,day:1)
         
         MovieNameLabel: while(true){
             print("Name of Movie you want to book is : ")
@@ -65,7 +67,7 @@ func CreateBooking(){
                 print("The movie you search doesn't exist! \n")
                 continue
             }else{
-                movieName = nameStr
+                customerName = nameStr
                 break CustomerNameLabel
             }
         }
@@ -76,33 +78,79 @@ func CreateBooking(){
                 print("Quantity cannot be empty! \n")
                 continue
             }
+            if(Int(quantityStr)! > ExistedMovie(name: movieName)!.quantity){
+                print("Sorry! The remaining quantity of this movie is only \(ExistedMovie(name: movieName)!.quantity) \n")
+                continue
+            }else{
+                ExistedMovie(name: movieName)!.quantity -= quantity
+            }
             quantity = Int(quantityStr)!
             break QuantityLabel
         }
         
-        BookingDateLabel:while(true){
-            print("Date you want to book from is : ")
-            guard let bookingDateStr:String = readLine(), !bookingDateStr.isEmpty else{
-                print("Booking Date cannot be empty! \n")
-                continue
+        DateLabel: while(true){
+            var day = ""
+            BookingDateLabel:while(true){
+                
+                print("Month you want to book from is : ")
+                guard let monthStr:String = readLine(), !monthStr.isEmpty else{
+                    print("Booking Month cannot be empty! \n")
+                    continue
+                }
+                while(true){
+                    print("Day you want to book from is : ")
+                    guard let dayStr:String = readLine(), !dayStr.isEmpty else{
+                        print("Booking Day cannot be empty! \n")
+                        continue
+                    }
+                    day = dayStr
+                    break
+                }
+                bookingDate = DateComponents(calendar: calendar, year: 2019, month: Int(monthStr)!, day:Int(day)!)
+                if(!bookingDate.isValidDate){
+                    print("Not valid date, please check the input")
+                    continue
+                }
+                break BookingDateLabel
             }
-            bookingDate = bookingDateStr
-            break BookingDateLabel
-        }
-        
-        ReturnDateLabel:while(true){
-            print("Date you want to return is : ")
-            guard let returnDateStr:String = readLine(), !returnDateStr.isEmpty else{
-                print("Booking Date cannot be empty! \n")
-                continue
+            
+            ReturnDateLabel:while(true){
+            
+                print("Month you want to book to is : ")
+                guard let monthStr:String = readLine(), !monthStr.isEmpty else{
+                    print("Return Month cannot be empty! \n")
+                    continue
+                }
+                while(true){
+                    print("Day you want to book to is : ")
+                    guard let dayStr:String = readLine(), !dayStr.isEmpty else{
+                        print("Return Day cannot be empty! \n")
+                        continue
+                    }
+                    day = dayStr
+                    break
+                }
+                returnDate = DateComponents(calendar: calendar, year: 2019, month: Int(monthStr)!, day:Int(day)!)
+                if(!returnDate.isValidDate){
+                    print("Not valid date, please check the input")
+                    continue
+                }
+                break ReturnDateLabel
             }
-            returnDate = returnDateStr
-            break ReturnDateLabel
+            
+            if(returnDate.month! < bookingDate.month!){
+                print("Error: end Date early than start Date, Choose Again")
+                continue DateLabel
+            }else if(returnDate.month! == bookingDate.month! && returnDate.day! < bookingDate.day!){
+                print("Error: end Date early than start Date, Choose Again")
+                continue DateLabel
+            }
+            break DateLabel
         }
         
         let booking = Booking()
-        booking.movie = ExistedMovie(name: movieName)
-        booking.customer = ExistedCustomer(name: customerName)
+        booking.movie = ExistedMovie(name: movieName)!
+        booking.customer = ExistedCustomer(name: customerName)!
         booking.quantity = quantity
         booking.bookingDate = bookingDate
         booking.returnDate = returnDate
@@ -115,10 +163,22 @@ func CreateBooking(){
 func ShowAllBookings(){
     if(BookingList.isEmpty){
         print("No booking in system now! \n")
+        while(true){
+            print("Do you want to add one booking now? (yes or no)")
+            guard let option:String = readLine(), !option.isEmpty else{
+                print("Please select one option! \n")
+                continue
+            }
+            if (option == "yes"){
+                CreateBooking()
+            }else{
+                break
+            }
+        }
     }else{
-        print("All Bookings are here : ")
+        print("----------------- All Bookings are here ------------------ ")
         for Booking in BookingList {
-            print("Id: \(Booking.id), Movie: \(Booking.movie), Customer: \(Booking.customer), Quantity: \(Booking.quantity), Booking Date: \(Booking.bookingDate), Return Date: \(Booking.returnDate) \n")
+            print("Id: \(Booking.id), Movie: \(Booking.movie!.name), Customer: \(Booking.customer!.name), Quantity: \(Booking.quantity!), Booking Date: \(Booking.bookingDate!.year!)-\(Booking.bookingDate!.month!)-\(Booking.bookingDate!.day!), Return Date: \(Booking.returnDate!.year!)-\(Booking.returnDate!.month!)-\(Booking.returnDate!.day!) \n")
         }
     }
 }
@@ -162,21 +222,23 @@ func UpdateBooking(){
         
         switch option{
         case "1":
+            ViewAllMovies()
             print("Name of the Movie: ")
             guard let nameStr:String = readLine(), !nameStr.isEmpty else{
                 print("Movie Name cannot be empty! \n")
                 continue
             }
-            targetBooking.movie = ExistedMovie(name: nameStr)
-            print("Booking's movie changed to \(targetBooking.movie) successfully! \n")
+            targetBooking.movie = ExistedMovie(name: nameStr)!
+            print("Booking's movie changed to \(targetBooking.movie!.name) successfully! \n")
         case "2":
+            DisplayAllCustomers()
             print("Name of the Customer: ")
             guard let nameStr:String = readLine(), !nameStr.isEmpty else{
                 print("Name of the Customer cannot be empty!")
                 continue
             }
-            targetBooking.customer = ExistedCustomer(name: nameStr)
-            print("Booking's customer changed to \(targetBooking.customer) successfully! \n")
+            targetBooking.customer = ExistedCustomer(name: nameStr)!
+            print("Booking's customer changed to \(targetBooking.customer!.name) successfully! \n")
         case "3":
             print("Quantity: ")
             guard let quantityStr:String = readLine(), !quantityStr.isEmpty,Int(quantityStr) != nil else{
@@ -184,23 +246,45 @@ func UpdateBooking(){
                 continue
             }
             targetBooking.quantity = Int(quantityStr)!
-            print("Booking's quantity changed to \(targetBooking.quantity) successfully!")
+            print("Booking's quantity changed to \(targetBooking.quantity!) successfully!")
         case "4":
             print("Booking Date: ")
-            guard let bookingDateStr:String = readLine(), !bookingDateStr.isEmpty else{
-                print("Booking Date cannot be empty! \n")
+            print("Month you want to book from is : ")
+            guard let monthStr:String = readLine(), !monthStr.isEmpty else{
+                print("Booking Month cannot be empty! \n")
                 continue
             }
-            targetBooking.bookingDate = bookingDateStr
-            print("Booking's start date changed to \(targetBooking.bookingDate) successfully! \n")
+            var day = ""
+            while(true){
+                print("Day you want to book from is : ")
+                guard let dayStr:String = readLine(), !dayStr.isEmpty else{
+                    print("Booking Day cannot be empty! \n")
+                    continue
+                }
+                day = dayStr
+                break
+            }
+            targetBooking.bookingDate = DateComponents(calendar: calendar, year: 2019, month: Int(monthStr)!, day:Int(day)!)
+            print("Booking's start date changed to \(targetBooking.bookingDate!.year!)-\(targetBooking.bookingDate!.month!)-\(targetBooking.bookingDate!.day!) successfully! \n")
         case "5":
             print("Return Date: ")
-            guard let returnDateStr:String = readLine(), !returnDateStr.isEmpty else{
-                print("Return Date cannot be empty! \n")
+            print("Month you want to book to is : ")
+            guard let monthStr:String = readLine(), !monthStr.isEmpty else{
+                print("Booking Month cannot be empty! \n")
                 continue
             }
-            targetBooking.returnDate = returnDateStr
-            print("Booking's return date changed to \(targetBooking.returnDate) successfully! \n")
+            var day = ""
+            while(true){
+                print("Day you want to book to is : ")
+                guard let dayStr:String = readLine(), !dayStr.isEmpty else{
+                    print("Booking Day cannot be empty! \n")
+                    continue
+                }
+                day = dayStr
+                break
+            }
+            targetBooking.returnDate = DateComponents(calendar: calendar, year: 2019, month: Int(monthStr)!, day:Int(day)!)
+            print("Booking's return date changed to \(targetBooking.returnDate!.year!)-\(targetBooking.returnDate!.month!)-\(targetBooking.returnDate!.day!) successfully! \n")
         default:
             print("The number you choose doesn't exist. Please follow the instruction \n")
             continue
@@ -210,22 +294,27 @@ func UpdateBooking(){
 }
 
 func DeleteBooking(){
-    ShowAllBookings()
-    while (true) {
-        print("Please input the id of the booking who you want to delete \n")
-        guard let id:String = readLine(), !id.isEmpty, Int(id) != nil else{
-            print("Id cannot be empty! \n")
-            continue
-        }
-        for Booking in BookingList{
-            if(Booking.id == Int(id)){
-                BookingList.remove(at:Booking.id-1)
-                print("Booking \(Booking.id) is deleted successfully! \n")
-            }else{
-                print("Booking doesn't exist \n")
-            }
-        }
         ShowAllBookings()
-        break
-    }
+        while (true) {
+            print("Please input the id of the booking who you want to delete \n")
+            guard let id:String = readLine(), !id.isEmpty, Int(id) != nil else{
+                print("Id cannot be empty! \n")
+                continue
+            }
+            if (ExistedBooking(id: Int(id)!) == nil){
+                print("Booking doesn't exist \n")
+                continue
+            }else{
+                for i in 0...BookingList.count-1{
+                    if(BookingList[i].id == Int(id)!){
+                        print(BookingList[i].id)
+                        BookingList.remove(at:i)
+                        print("Booking No. \(id) is deleted successfully! \n")
+                        ShowAllBookings()
+                        break
+                    }
+                }
+            }
+            break
+        }
 }
